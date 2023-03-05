6ea518570d48e9b329b5dac24d10bf310ffe2e0e
Athi <athithyanr4@gmail.com>
2023-03-05 09:06:10 +0530

    invalid commit message

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os/exec"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -20,6 +21,15 @@ func main() {
 		}
 		stringOutput := string(output)
 		return c.SendString(stringOutput)
+	})
+
+	app.Get("/return-slowly", func(c *fiber.Ctx) error {
+		ticker := time.NewTicker(1 * time.Second)
+		for i := 0; i < 5; i++ {
+			c.Write([]byte(time.Now().String()))
+			<-ticker.C
+		}
+		return nil
 	})
 
 	log.Fatal(app.Listen(":6969"))
